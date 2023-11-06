00c3778c7bb77650db4720d1c7ebd1a6d882ea54
Marco <115066181+MarcoRioja@users.noreply.github.com>
2023-11-06 20:28:21 +0100

    ReUPload

---
+++ Unity/4 - My Balls/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float velocidadRotacion = 50;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float entradaHorizontal = Input.GetAxis("Horizontal");
+
+
+        transform.Rotate(Vector3.up,velocidadRotacion*Time.deltaTime*entradaHorizontal);
+
+
+    }
+}
